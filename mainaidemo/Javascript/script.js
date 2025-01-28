fd3e18fbb954990d675556e3a5f8aee4cf078aa9
harshadteli <harshadteli46@gmail.com>
2025-01-28 09:11:19 +0530

    Add files via upload

---
+++ mainaidemo/Javascript/script.js
@@ -0,0 +1,64 @@
+const textchatbot=document.querySelector(".sec-textchat");
+            const textLoad1 = () =>{
+                setTimeout( () => {
+                    textchatbot.textContent="We Provide Some Web Virtual Assistent's......";
+                },0);
+                setTimeout(()=>{
+                    textchatbot.textContent="Communicate Your Virtual Assistent's...";
+                },4000);
+                setTimeout(()=>{
+                    textchatbot.textContent="JARVIS is an AI agent to automate Web-Tasked tasks.....";
+                },8000);
+                setTimeout(()=>{
+                    textchatbot.textContent="Mezo is Application of AI which Perform Basic Task........";
+                },12000);
+                setTimeout(()=>{
+                    textchatbot.textContent="Natasha is an AI Application to Perform on User Inputs......";
+                },16000);
+            }
+            textLoad1();
+            setInterval(textLoad1,20000);
+const text=document.querySelector(".sec-text");
+const textLoad = () =>{
+    setTimeout( () => {
+        text.textContent="Welcome To........";
+    },0);
+    setTimeout(()=>{
+        text.textContent="Artificial Intelligence and Data Science..";
+    },4000);
+    setTimeout(()=>{
+        text.textContent="Move to New Another World.....";
+    },8000);
+    setTimeout(()=>{
+        text.textContent="Various AI Tools is Just For you....";
+    },12000);
+}
+textLoad();
+setInterval(textLoad,16000);
+
+const textchatbot1=document.querySelector(".sec-textchat1");
+const textLoad2 = () =>{
+    setTimeout( () => {
+        textchatbot1.textContent="Welcome To Chatbot......";
+    },0);
+    setTimeout(()=>{
+        textchatbot1.textContent="An open Source chatbot Platform....";
+    },4000);
+    setTimeout(()=>{
+        textchatbot1.textContent="Users to install their own Chatbot...";
+    },8000);
+    setTimeout(()=>{
+        textchatbot1.textContent="Siri Apples's iphone bot";
+    },12000);
+    setTimeout(()=>{
+        textchatbot1.textContent="Genny is ai chatbot..";
+    },16000);
+    setTimeout(()=>{
+        textchatbot1.textContent="Maths Assistent-AI Tool....";
+    },20000);
+    setTimeout(()=>{
+        textchatbot1.textContent="You Can Easily Chat with us.....";
+    },24000);
+}
+textLoad2();
+setInterval(textLoad2,28000);
